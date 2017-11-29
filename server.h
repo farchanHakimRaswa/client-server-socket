3839c7fdad778fcb8da177fa54a4ff2d1a3b6528
farchanHakimRaswa <34081694+farchanHakimRaswa@users.noreply.github.com>
2017-11-29 10:10:59 +0700

    Add files via upload

---
+++ server.h
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+char buffer[1024];
+struct sockaddr_in serverAddr, clientAddr;
+struct sockaddr_storage serverStorage;
+socklen_t addr_size, client_addr_size;
+int i;
+int udpSocket, nBytes;
+
+
+void create_server_socket(char *ip, int port) {
+  
+
+  /*Create UDP socket*/
+  udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
+
+  /*Configure settings in address struct*/
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(port);
+  serverAddr.sin_addr.s_addr = inet_addr(ip);
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+  /*Bind socket with address struct*/
+  bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+  /*Initialize size variable to be used later on*/
+  addr_size = sizeof serverStorage;
+
+}
+
+char *recv_data() {
+
+   recvfrom(udpSocket,buffer,1024,0,(struct sockaddr *)&serverStorage, &addr_size);
+   return buffer;
+}
+
+void send_data(char *buffer) {
+   sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
+}
+
+void inisialisasi() {
+	char *data;	
+	data = recv_data();
+	printf(" data %s \n", data);
+}
